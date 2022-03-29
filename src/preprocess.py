aa56cbc7346c19c69f6d1c5cd981b83d7a3fab9a
Julien <julien.aubuchon@gmail.com>
2022-03-29 11:00:38 -0400

    Update preprocess

--- src/preprocess.py
+++ src/preprocess.py
@@ -5,6 +5,9 @@ class PreprocessAbstract():
     def __init__(self):
         pass
 
+    def onlyPicturesPosts(self, df):
+        return df.loc[df['type'] == 'Photo']
+
 
 class PreprocessVis1(PreprocessAbstract):
     def __init__(self):
@@ -16,36 +19,19 @@ class PreprocessVis4(PreprocessAbstract):
     def __init__(self):
         pass
 
-    def savePostImages(self):
-        from selenium import webdriver
-        from selenium.webdriver.chrome.options import Options
-        import requests
+    def savePostImages(self, df):
 
-        chrome_options = Options()
-        chrome_options.add_argument("--headless")
-        driver = webdriver.Chrome(options=chrome_options)
-        driver.implicitly_wait(1)
-        driver.maximize_window()
-
-        df = pd.read_csv('../data/data.csv')
-        df_most_likes = df.head(20).sort_values('likes')
+        df_most_likes = df.sort_values('likes').head(100)
 
         all_images_url = df_most_likes['url']
 
         for i, url in enumerate(all_images_url):
-            # Get image
-            driver.get(url)
-            driver.implicitly_wait(2)
-            image_div = driver.find_element_by_class_name('FFVAD')
-            image_src = image_div.get_attribute('src')
-            print(image_src)
-            # Save image
-            img_data = requests.get(image_src).content
-            with open(f'../data/images/image_{i}.jpg', 'wb') as handler:
-                handler.write(img_data)
-            time.sleep(5)
+            print(f"{i}: {url}")
+
 
 
 if __name__ == "__main__":
+    df = pd.read_csv('../data/data.csv')
     a = PreprocessVis4()
-    a.savePostImages()
+    df = a.onlyPicturesPosts(df)
+    a.savePostImages(df)
